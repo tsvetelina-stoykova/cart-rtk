3d4b3f20f847a889f3c210866c0cb11f6d28b4d9
tsevetelina-stoykova <cveti.stoikova@gmail.com>
2022-08-01 17:34:58 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import Layout from './components/Layout/Layout'
 import Cart from './components/Cart/Cart';
+import Products from './components/Shop/Products'
+
+
 function App() {
-  return (
-    <div className="App">
-		<Layout>
-			<Cart/>
-		</Layout>
-    </div>
-  );
+	const showCart = useSelector(state => state.ui.isCartVisible)
+	const cart = useSelector((state) => state.cart)
+
+	useEffect(() => {
+		fetch('https://react-http-2354a-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+			method: 'PUT',
+			body: JSON.stringify(cart)
+		})
+	}, [cart])
+
+	return (
+		<div className="App">
+			<Layout>
+				{showCart && <Cart/>}
+				<Products />
+			</Layout>
+		</div>
+	  );
 }
 
 export default App;
--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,18 @@
 import classes from './cartButton.module.css';
+import {uiActions} from '../../store/ui-slice'
+import { useDispatch, useSelector } from 'react-redux';
 
 const CartButton = (props) => {
+	const dispatch = useDispatch()
+	const cartQuantity = useSelector(state => state.cart.totalQuantity)
+	const toggleCartHandler = () => {
+		dispatch(uiActions.toggleCart());
+	}
+
   return (
-    <button className={classes.button}>
+    <button onClick = {toggleCartHandler} className={classes.button}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
